0cedc033119ec689a36d30dcbc1e5d7799e94542
deepika proz <jdeepika@proztec.com>
2023-05-21 01:55:47 -0700

    Test 3

--- resources/views/includes/company_dashboard_menu.blade.php
+++ resources/views/includes/company_dashboard_menu.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-3 col-sm-4">
 	<div class="usernavwrap">
     <ul class="usernavdash">
-        <li class="{{ Request::url() == route('company.home') ? 'active' : '' }}"><a href="{{route('company.home')}}"><i class="fa fa-tachometer" aria-hidden="true"></i> {{__('Dashboard')}}</a></li>
+        <li class="{{ Request::url() == route('company.home') ? 'active' : '' }}"><a href="{{route('company.home')}}"><i class="fa fa-tachometer" aria-hidden="true"></i> {{__('Hello Dashboard')}}</a></li>
         <li class="{{ Request::url() == route('company.profile') ? 'active' : '' }}"><a href="{{ route('company.profile') }}"><i class="fa fa-pencil" aria-hidden="true"></i> {{__('Edit Profile')}}</a></li>
         <li><a href="{{ route('company.detail', Auth::guard('company')->user()->slug) }}"><i class="fa fa-user" aria-hidden="true"></i> {{__('Public Profile')}}</a></li>
         <li class="{{ Request::url() == route('post.job') ? 'active' : '' }}"><a href="{{ route('post.job') }}"><i class="fa fa-desktop" aria-hidden="true"></i> {{__('Post New Job')}}</a></li>
