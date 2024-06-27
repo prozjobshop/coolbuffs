41928b623b2dd81ec4db1c230d6877d82a662b08
ivanchristian23 <127865744+ivanchristian23@users.noreply.github.com>
2024-06-27 15:14:31 +0300

    nothing

--- app/Traits/CompanyPackageTrait.php
+++ app/Traits/CompanyPackageTrait.php
@@ -21,7 +21,6 @@ trait CompanyPackageTrait
         $company->availed_jobs_quota = 0;
         $company->payment_method = $method;
         $company->availed_viewed_resume_quota = 0;
-        
         $company->update();
     }
 
